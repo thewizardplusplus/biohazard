30efbaf212496a16822312ecaec97186ae5f1817
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-05 23:00:00 +0300

    Update the lualife library

--- vendor/lualife/random.lua
+++ vendor/lualife/random.lua
@@ -4,6 +4,15 @@
 local Size = require("lualife.models.size")
 local Field = require("lualife.models.field")
 
+local function length(table)
+  local count = 0
+  for _ in pairs(table) do
+    count = count + 1
+  end
+
+  return count
+end
+
 local random = {}
 
 ---
@@ -19,6 +28,33 @@ function random.generate(size, filling)
     :map(function()
       return math.random() < filling
     end)
+end
+
+---
+-- @tparam Size size
+-- @tparam number filling [0, 1]
+-- @tparam int minimal_count [0, size.width * size.height]
+-- @tparam int maximal_count [0, size.width * size.height]
+-- @treturn Field
+function random.generate_with_limits(
+  size,
+  filling,
+  minimal_count,
+  maximal_count
+)
+  assert(size:isInstanceOf(Size))
+  assert(type(filling) == "number")
+  assert(type(minimal_count) == "number")
+  assert(type(maximal_count) == "number")
+
+  local field = Field:new(size)
+  local count = 0
+  while count < minimal_count or count > maximal_count do
+    field = random.generate(size, filling)
+    count = length(field.cells)
+  end
+
+  return field
 end
 
 return random
