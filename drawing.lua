94816927a8049291a4a3b0ccccbcaa39b41ff4bc
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-09-04 23:00:00 +0300

    Add the drawing.draw_cell() function

--- drawing.lua
+++ drawing.lua
@@ -68,26 +68,46 @@ function drawing.draw_field(
     assert(types.is_number_with_limits(cell_color[index], 0, 1))
   end
 
-  local cell_radius = CELL_RADIUS_FACTOR * grid_step
   field:map(function(point, contains)
-    if not contains then
-      return
+    if contains then
+      drawing.draw_cell(point, field_offset, grid_step, cell_color)
     end
-
-    local cell_point = point
-      :scale(grid_step)
-      :translate(field_offset)
-
-    love.graphics.setColor(cell_color)
-    love.graphics.rectangle(
-      "fill",
-      cell_point.x,
-      cell_point.y,
-      grid_step,
-      grid_step,
-      cell_radius
-    )
   end)
+end
+
+---
+-- @tparam lualife.models.Point point
+-- @tparam lualife.models.Point field_offset
+-- @tparam int grid_step [0, ∞)
+-- @tparam {number,number,number} cell_color
+--   red, green and blue values in the range [0, 1]
+function drawing.draw_cell(
+  point,
+  field_offset,
+  grid_step,
+  cell_color
+)
+  assert(types.is_instance(point, Point))
+  assert(types.is_instance(field_offset, Point))
+  assert(types.is_number_with_limits(grid_step, 0))
+  assert(type(cell_color) == "table" and #cell_color == 3)
+  for index in ipairs(cell_color) do
+    assert(types.is_number_with_limits(cell_color[index], 0, 1))
+  end
+
+  local cell_point = point
+    :scale(grid_step)
+    :translate(field_offset)
+
+  love.graphics.setColor(cell_color)
+  love.graphics.rectangle(
+    "fill",
+    cell_point.x,
+    cell_point.y,
+    grid_step,
+    grid_step,
+    CELL_RADIUS_FACTOR * grid_step
+  )
 end
 
 return drawing
