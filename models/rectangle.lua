533d22195dfb34d817be9c5ae07b3b81580fa307
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-09-05 23:00:00 +0300

    Add the Rectangle class

---
+++ models/rectangle.lua
@@ -0,0 +1,28 @@
+---
+-- @classmod Rectangle
+
+local middleclass = require("middleclass")
+local types = require("lualife.types")
+local Point = require("lualife.models.point")
+
+local Rectangle = middleclass("Rectangle")
+
+---
+-- @table instance
+-- @tfield lualife.models.Point minimum
+-- @tfield lualife.models.Point maximum
+
+---
+-- @function new
+-- @tparam lualife.models.Point minimum
+-- @tparam lualife.models.Point maximum
+-- @treturn Rectangle
+function Rectangle:initialize(minimum, maximum)
+  assert(types.is_instance(minimum, Point))
+  assert(types.is_instance(maximum, Point))
+
+  self.minimum = minimum
+  self.maximum = maximum
+end
+
+return Rectangle
