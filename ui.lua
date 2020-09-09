f9e0063b034c0f6dd13906bfe1baea0ab0b36715
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-09-09 23:00:00 +0300

    Fix labels options

--- ui.lua
+++ ui.lua
@@ -9,6 +9,24 @@ local Rectangle = require("models.rectangle")
 local Stats = require("models.stats")
 local UiUpdate = require("models.uiupdate")
 
+-- @tparam {number,number,number} color
+--   red, green and blue values in the range [0, 1] 
+-- @tparam "left"|"right" align
+-- @treturn tab common suit widget options
+local function make_options(color, align)
+  assert(type(color) == "table" and #color == 3)
+  for _, channel in ipairs(color) do
+    assert(types.is_number_with_limits(channel, 0, 1))
+  end
+  assert(align == "left" or align == "right")
+
+  return {
+    color = {normal = {fg = color}},
+    align = align,
+    valign = "top",
+  }
+end
+
 local ui = {}
 
 ---
@@ -50,23 +68,17 @@ function ui.update(screen, stats)
   local to_top_button = suit.Button("^", suit.layout:row())
   local to_bottom_button = suit.Button("v", suit.layout:row())
 
-  local now_options = {
-    color = {normal = {fg = {0, 0, 0}}},
-    align = "right",
-    valign = "top",
-  }
   suit.layout:reset(labels_offset.x, labels_offset.y, padding)
-  suit.Label("Now:", now_options, suit.layout:row(2 * grid_step / 3, grid_step / 3))
-  suit.Label(tostring(stats.current), now_options, suit.layout:col(grid_step / 3, grid_step / 3))
+  local current_title_options = make_options({0, 0, 0}, "left")
+  suit.Label("Now:", current_title_options, suit.layout:row(2 * grid_step / 3, grid_step / 3))
+  local current_value_options = make_options({0, 0, 0}, "right")
+  suit.Label(tostring(stats.current), current_value_options, suit.layout:col(grid_step / 3, grid_step / 3))
 
-  local min_options = {
-    color = {normal = {fg = {0, 0.33, 0}}},
-    align = "right",
-    valign = "top",
-  }
   suit.layout:reset(labels_offset.x, labels_offset.y + grid_step / 3 + padding, padding)
-  suit.Label("Min:", min_options, suit.layout:row(2 * grid_step / 3, grid_step / 3))
-  suit.Label(tostring(stats.minimal), min_options, suit.layout:col(grid_step / 3, grid_step / 3))
+  local minimal_title_options = make_options({0, 0.33, 0}, "left")
+  suit.Label("Min:", minimal_title_options, suit.layout:row(2 * grid_step / 3, grid_step / 3))
+  local minimal_value_options = make_options({0, 0.33, 0}, "right")
+  suit.Label(tostring(stats.minimal), minimal_value_options, suit.layout:col(grid_step / 3, grid_step / 3))
 
   local delta_offset = Point:new(0, 0)
   if to_left_button.hit then
