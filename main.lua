c0134ece544673959c0ffb8fe3cd05a0e0e6b2b7
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-09-20 23:00:00 +0300

    Add control via a keyboard

--- main.lua
+++ main.lua
@@ -10,6 +10,7 @@ local FieldSettings = require("biohazardcore.models.fieldsettings")
 local GameSettings = require("biohazardcore.models.gamesettings")
 local ClassifiedGame = require("biohazardcore.classifiedgame")
 local Rectangle = require("models.rectangle")
+local UiUpdate = require("models.uiupdate")
 local drawing = require("drawing")
 local ui = require("ui")
 local updating = require("updating")
@@ -64,4 +65,19 @@ function love.keypressed(key)
   if key == "escape" then
     love.event.quit()
   end
+
+  local moved_left = key == "left" or key == "a"
+  local moved_right = key == "right" or key == "d"
+  local moved_top = key == "up" or key == "w"
+  local moved_bottom = key == "down" or key == "s"
+  local rotated = key == "lshift" or key == "rshift" or key == "r"
+  local unioned = key == "space"
+  updating.update_game(game, UiUpdate:new(
+    moved_left,
+    moved_right,
+    moved_top,
+    moved_bottom,
+    rotated,
+    unioned
+  ))
 end
