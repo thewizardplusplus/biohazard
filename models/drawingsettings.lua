c255cecb24914b0d53b0f9e15c19cc84abf3c66a
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-09-23 23:00:00 +0300

    Add the DrawingSettings model

---
+++ models/drawingsettings.lua
@@ -0,0 +1,33 @@
+---
+-- @classmod DrawingSettings
+
+local middleclass = require("middleclass")
+local types = require("lualife.types")
+local Point = require("lualife.models.point")
+local CellClassification = require("biohazardcore.models.cellclassification")
+
+local DrawingSettings = middleclass("DrawingSettings")
+
+---
+-- @table instance
+-- @tfield lualife.models.Point field_offset
+-- @tfield int grid_step [0, ∞)
+-- @tfield "old"|"new"|"intersection" cell_kind
+
+---
+-- @function new
+-- @tparam lualife.models.Point field_offset
+-- @tparam int grid_step [0, ∞)
+-- @tparam "old"|"new"|"intersection" cell_kind
+-- @treturn DrawingSettings
+function DrawingSettings:initialize(field_offset, grid_step, cell_kind)
+  assert(types.is_instance(field_offset, Point))
+  assert(types.is_number_with_limits(grid_step, 0))
+  assert(CellClassification.is_cell_kind(cell_kind))
+
+  self.field_offset = field_offset
+  self.grid_step = grid_step
+  self.cell_kind = cell_kind
+end
+
+return DrawingSettings
