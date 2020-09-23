cb920ef9ce836f49e9ac451740de2990ba472aaf
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-09-23 23:00:00 +0300

    Make the DrawingSettings:cell_kind field optional

--- models/drawingsettings.lua
+++ models/drawingsettings.lua
@@ -12,22 +12,31 @@ local DrawingSettings = middleclass("DrawingSettings")
 -- @table instance
 -- @tfield lualife.models.Point field_offset
 -- @tfield int grid_step [0, ∞)
--- @tfield "old"|"new"|"intersection" cell_kind
+-- @tfield[opt] "old"|"new"|"intersection" cell_kind
 
 ---
 -- @function new
 -- @tparam lualife.models.Point field_offset
 -- @tparam int grid_step [0, ∞)
--- @tparam "old"|"new"|"intersection" cell_kind
+-- @tparam[opt] "old"|"new"|"intersection" cell_kind
 -- @treturn DrawingSettings
 function DrawingSettings:initialize(field_offset, grid_step, cell_kind)
   assert(types.is_instance(field_offset, Point))
   assert(types.is_number_with_limits(grid_step, 0))
-  assert(CellClassification.is_cell_kind(cell_kind))
+  assert(cell_kind == nil or CellClassification.is_cell_kind(cell_kind))
 
   self.field_offset = field_offset
   self.grid_step = grid_step
   self.cell_kind = cell_kind
+end
+
+---
+-- @tparam "old"|"new"|"intersection" cell_kind
+-- @treturn DrawingSettings
+function DrawingSettings:with_cell_kind(cell_kind)
+  assert(CellClassification.is_cell_kind(cell_kind))
+
+  return DrawingSettings(self.field_offset, self.grid_step, cell_kind)
 end
 
 return DrawingSettings
