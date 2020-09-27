5c59ed2219966d2a86bf16ecf20b82d98450928d
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-09-27 23:00:00 +0300

    Add the set_title() function

--- conf.lua
+++ conf.lua
@@ -1,13 +1,17 @@
+local function set_title(config, title)
+  config.window.title = title
+  config.identity = string.lower(title)
+end
+
 function love.conf(config)
-  local title = "Biohazard"
   local aspect_ratio = 16 / 10
   config.version = "11.3"
-  config.identity = string.lower(title)
-  config.window.title = title
   config.window.width = 640
   config.window.height = config.window.width / aspect_ratio
   config.window.minwidth = 640
   config.window.minheight = config.window.minheight / aspect_ratio
   config.window.resizable = true
   config.window.msaa = 8
+
+  set_title(config, "Biohazard")
 end
