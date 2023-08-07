4c034e191aae67bcf354fe5e63dae16645f9d840
thewizardplusplus <thewizardplusplus@yandex.ru>
2023-08-08 02:39:06 +0600

    Add the `luatypechecks` package

---
+++ vendor/luatypechecks/assertions.lua
@@ -0,0 +1,248 @@
+-- luacheck: no max comment line length
+
+---
+-- @module assertions
+
+local checks = require("luatypechecks.checks")
+
+local _assertions_mode = "with_assertions"
+
+local assertions = {}
+local _assertions_backup = {}
+
+---
+-- @treturn "without_assertions"|"with_assertions"
+function assertions.get_assertions_mode()
+  return _assertions_mode
+end
+
+---
+-- @tparam "without_assertions"|"with_assertions" value
+function assertions.set_assertions_mode(value)
+  assertions.is_assertions_mode(value)
+
+  _assertions_mode = value
+
+  -- switch on/off functions that start with "is_"
+  for key, _ in pairs(_assertions_backup) do
+    if not string.match(key, "^is_") then
+      goto end_of_iteration
+    end
+
+    local value = _assertions_backup[key] -- luacheck: no redefined
+    if _assertions_mode == "without_assertions" then
+      value = function() end
+    end
+
+    assertions[key] = value
+
+    ::end_of_iteration::
+  end
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function assertions.is_deep_checks_mode(value)
+  assert(checks.is_deep_checks_mode(value))
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function assertions.is_deep_checks_mode_or_nil(value)
+  assert(checks.is_deep_checks_mode_or_nil(value))
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function assertions.is_assertions_mode(value)
+  assert(checks.is_assertions_mode(value))
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function assertions.is_assertions_mode_or_nil(value)
+  assert(checks.is_assertions_mode_or_nil(value))
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function assertions.is_boolean(value)
+  assert(checks.is_boolean(value))
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function assertions.is_boolean_or_nil(value)
+  assert(checks.is_boolean_or_nil(value))
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function assertions.is_number(value)
+  assert(checks.is_number(value))
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function assertions.is_number_or_nil(value)
+  assert(checks.is_number_or_nil(value))
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function assertions.is_integer(value)
+  assert(checks.is_integer(value))
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function assertions.is_integer_or_nil(value)
+  assert(checks.is_integer_or_nil(value))
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function assertions.is_string(value)
+  assert(checks.is_string(value))
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function assertions.is_string_or_nil(value)
+  assert(checks.is_string_or_nil(value))
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function assertions.is_function(value)
+  assert(checks.is_function(value))
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function assertions.is_function_or_nil(value)
+  assert(checks.is_function_or_nil(value))
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function assertions.is_callable(value)
+  assert(checks.is_callable(value))
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function assertions.is_callable_or_nil(value)
+  assert(checks.is_callable_or_nil(value))
+end
+
+---
+-- @tparam any value
+-- @tparam[opt] func key_checker `func(value: any): bool`
+-- @tparam[optchain] func value_checker `func(value: any): bool`
+-- @tparam[optchain="with_deep_checks"] "without_deep_checks"|"with_deep_checks" deep_checks_mode
+-- @treturn bool
+function assertions.is_table(
+  value,
+  key_checker,
+  value_checker,
+  deep_checks_mode
+)
+  assert(checks.is_table(value, key_checker, value_checker, deep_checks_mode))
+end
+
+---
+-- @tparam any value
+-- @tparam[opt] func key_checker `func(value: any): bool`
+-- @tparam[optchain] func value_checker `func(value: any): bool`
+-- @tparam[optchain="with_deep_checks"] "without_deep_checks"|"with_deep_checks" deep_checks_mode
+-- @treturn bool
+function assertions.is_table_or_nil(
+  value,
+  key_checker,
+  value_checker,
+  deep_checks_mode
+)
+  assert(checks.is_table_or_nil(
+    value,
+    key_checker,
+    value_checker,
+    deep_checks_mode
+  ))
+end
+
+---
+-- @tparam any value
+-- @tparam[opt] func value_checker `func(value: any): bool`
+-- @tparam[optchain="with_deep_checks"] "without_deep_checks"|"with_deep_checks" deep_checks_mode
+-- @treturn bool
+function assertions.is_sequence(value, value_checker, deep_checks_mode)
+  assert(checks.is_sequence(value, value_checker, deep_checks_mode))
+end
+
+---
+-- @tparam any value
+-- @tparam[opt] func value_checker `func(value: any): bool`
+-- @tparam[optchain="with_deep_checks"] "without_deep_checks"|"with_deep_checks" deep_checks_mode
+-- @treturn bool
+function assertions.is_sequence_or_nil(value, value_checker, deep_checks_mode)
+  assert(checks.is_sequence_or_nil(value, value_checker, deep_checks_mode))
+end
+
+---
+-- @tparam any value
+-- @tparam {any,...} enumeration
+-- @treturn bool
+function assertions.is_enumeration(value, enumeration)
+  assert(checks.is_enumeration(value, enumeration))
+end
+
+---
+-- @tparam any value
+-- @tparam {any,...} enumeration
+-- @treturn bool
+function assertions.is_enumeration_or_nil(value, enumeration)
+  assert(checks.is_enumeration_or_nil(value, enumeration))
+end
+
+---
+-- @tparam any value
+-- @tparam tab class a class created by library [middleclass](https://github.com/kikito/middleclass)
+-- @treturn bool
+function assertions.is_instance(value, class)
+  assert(checks.is_instance(value, class))
+end
+
+---
+-- @tparam any value
+-- @tparam tab class a class created by library [middleclass](https://github.com/kikito/middleclass)
+-- @treturn bool
+function assertions.is_instance_or_nil(value, class)
+  assert(checks.is_instance_or_nil(value, class))
+end
+
+-- we cannot check right away because at that moment the check function isn't defined
+assertions.is_assertions_mode(_assertions_mode)
+
+-- after filling module `assertions`, we make a backup of it
+for key, value in pairs(assertions) do
+  _assertions_backup[key] = value
+end
+
+return assertions
---
+++ vendor/luatypechecks/checks.lua
@@ -0,0 +1,392 @@
+-- luacheck: no max comment line length
+
+---
+-- @module checks
+
+local _global_deep_checks_mode = "with_deep_checks"
+
+local checks = {}
+
+---
+-- @treturn "without_deep_checks"|"with_deep_checks"
+function checks.get_global_deep_checks_mode()
+  return _global_deep_checks_mode
+end
+
+---
+-- @tparam "without_deep_checks"|"with_deep_checks" value
+function checks.set_global_deep_checks_mode(value)
+  assert(checks.is_deep_checks_mode(value))
+
+  _global_deep_checks_mode = value
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_deep_checks_mode(value)
+  -- we cannot use `checks.is_enumeration()` due to recursion
+  return value == "without_deep_checks" or value == "with_deep_checks"
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_deep_checks_mode_or_nil(value)
+  return checks.is_deep_checks_mode(value) or value == nil
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_assertions_mode(value)
+  return checks.is_enumeration(value, {"without_assertions", "with_assertions"})
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_assertions_mode_or_nil(value)
+  return checks.is_assertions_mode(value) or value == nil
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_boolean(value)
+  return type(value) == "boolean"
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_boolean_or_nil(value)
+  return checks.is_boolean(value) or value == nil
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_number(value)
+  return type(value) == "number"
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_number_or_nil(value)
+  return checks.is_number(value) or value == nil
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_integer(value)
+  if not checks.is_number(value) then
+    return false
+  end
+
+  local value_integral_part, _ = math.modf(value)
+  return value == value_integral_part
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_integer_or_nil(value)
+  return checks.is_integer(value) or value == nil
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_string(value)
+  return type(value) == "string"
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_string_or_nil(value)
+  return checks.is_string(value) or value == nil
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_function(value)
+  return type(value) == "function"
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_function_or_nil(value)
+  return checks.is_function(value) or value == nil
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_callable(value)
+  if checks.is_function(value) then
+    return true
+  end
+
+  local metatable = getmetatable(value)
+  return metatable ~= nil
+    and metatable.__call ~= nil
+    and checks.is_callable(metatable.__call)
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_callable_or_nil(value)
+  return checks.is_callable(value) or value == nil
+end
+
+---
+-- @tparam any value
+-- @tparam[opt] func key_checker `func(value: any): bool`
+-- @tparam[optchain] func value_checker `func(value: any): bool`
+-- @tparam[optchain="with_deep_checks"] "without_deep_checks"|"with_deep_checks" deep_checks_mode
+-- @treturn bool
+function checks.is_table(value, key_checker, value_checker, deep_checks_mode)
+  deep_checks_mode = deep_checks_mode or "with_deep_checks"
+
+  assert(checks.is_function_or_nil(key_checker))
+  assert(checks.is_function_or_nil(value_checker))
+  assert(checks.is_deep_checks_mode(deep_checks_mode))
+
+  if type(value) ~= "table" then
+    return false
+  end
+  if key_checker == nil and value_checker == nil then
+    return true
+  end
+  if checks._without_deep_checks(deep_checks_mode) then
+    return true
+  end
+
+  for key, value in pairs(value) do -- luacheck: no redefined
+    if key_checker ~= nil and not key_checker(key) then
+      return false
+    end
+    if value_checker ~= nil and not value_checker(value) then
+      return false
+    end
+  end
+
+  return true
+end
+
+---
+-- @tparam[opt] func key_checker `func(value: any): bool`
+-- @tparam[optchain] func value_checker `func(value: any): bool`
+-- @tparam[optchain="with_deep_checks"] "without_deep_checks"|"with_deep_checks" deep_checks_mode
+-- @treturn func `func(value: any): bool`
+function checks.make_table_checker(key_checker, value_checker, deep_checks_mode)
+  return function(value)
+    return checks.is_table(value, key_checker, value_checker, deep_checks_mode)
+  end
+end
+
+---
+-- @tparam any value
+-- @tparam[opt] func key_checker `func(value: any): bool`
+-- @tparam[optchain] func value_checker `func(value: any): bool`
+-- @tparam[optchain="with_deep_checks"] "without_deep_checks"|"with_deep_checks" deep_checks_mode
+-- @treturn bool
+function checks.is_table_or_nil(
+  value,
+  key_checker,
+  value_checker,
+  deep_checks_mode
+)
+  return checks.is_table(value, key_checker, value_checker, deep_checks_mode)
+    or value == nil
+end
+
+---
+-- @tparam[opt] func key_checker `func(value: any): bool`
+-- @tparam[optchain] func value_checker `func(value: any): bool`
+-- @tparam[optchain="with_deep_checks"] "without_deep_checks"|"with_deep_checks" deep_checks_mode
+-- @treturn func `func(value: any): bool`
+function checks.make_table_or_nil_checker(
+  key_checker,
+  value_checker,
+  deep_checks_mode
+)
+  return function(value)
+    return checks.is_table_or_nil(
+      value,
+      key_checker,
+      value_checker,
+      deep_checks_mode
+    )
+  end
+end
+
+---
+-- @tparam any value
+-- @tparam[opt] func value_checker `func(value: any): bool`
+-- @tparam[optchain="with_deep_checks"] "without_deep_checks"|"with_deep_checks" deep_checks_mode
+-- @treturn bool
+function checks.is_sequence(value, value_checker, deep_checks_mode)
+  deep_checks_mode = deep_checks_mode or "with_deep_checks"
+
+  assert(checks.is_function_or_nil(value_checker))
+  assert(checks.is_deep_checks_mode(deep_checks_mode))
+
+  if not checks.is_table(
+    value,
+    checks.is_number,
+    value_checker,
+    deep_checks_mode
+  ) then
+    return false
+  end
+  if checks._without_deep_checks(deep_checks_mode) then
+    return true
+  end
+
+  local indices = {}
+  for index, _ in pairs(value) do
+    table.insert(indices, index)
+  end
+  table.sort(indices)
+
+  local expected_index = 1
+  for _, actual_index in ipairs(indices) do
+    if actual_index ~= expected_index then
+      return false
+    end
+
+    expected_index = expected_index + 1
+  end
+
+  return true
+end
+
+---
+-- @tparam[opt] func value_checker `func(value: any): bool`
+-- @tparam[optchain="with_deep_checks"] "without_deep_checks"|"with_deep_checks" deep_checks_mode
+-- @treturn func `func(value: any): bool`
+function checks.make_sequence_checker(value_checker, deep_checks_mode)
+  return function(value)
+    return checks.is_sequence(value, value_checker, deep_checks_mode)
+  end
+end
+
+---
+-- @tparam any value
+-- @tparam[opt] func value_checker `func(value: any): bool`
+-- @tparam[optchain="with_deep_checks"] "without_deep_checks"|"with_deep_checks" deep_checks_mode
+-- @treturn bool
+function checks.is_sequence_or_nil(value, value_checker, deep_checks_mode)
+  return checks.is_sequence(value, value_checker, deep_checks_mode)
+    or value == nil
+end
+
+---
+-- @tparam[opt] func value_checker `func(value: any): bool`
+-- @tparam[optchain="with_deep_checks"] "without_deep_checks"|"with_deep_checks" deep_checks_mode
+-- @treturn func `func(value: any): bool`
+function checks.make_sequence_or_nil_checker(value_checker, deep_checks_mode)
+  return function(value)
+    return checks.is_sequence_or_nil(value, value_checker, deep_checks_mode)
+  end
+end
+
+---
+-- @tparam any value
+-- @tparam {any,...} enumeration
+-- @treturn bool
+function checks.is_enumeration(value, enumeration)
+  assert(checks.is_sequence(enumeration))
+
+  for _, element in ipairs(enumeration) do
+    if value == element then
+      return true
+    end
+  end
+
+  return false
+end
+
+---
+-- @tparam {any,...} enumeration
+-- @treturn func `func(value: any): bool`
+function checks.make_enumeration_checker(enumeration)
+  return function(value)
+    return checks.is_enumeration(value, enumeration)
+  end
+end
+
+---
+-- @tparam any value
+-- @tparam {any,...} enumeration
+-- @treturn bool
+function checks.is_enumeration_or_nil(value, enumeration)
+  return checks.is_enumeration(value, enumeration) or value == nil
+end
+
+---
+-- @tparam {any,...} enumeration
+-- @treturn func `func(value: any): bool`
+function checks.make_enumeration_or_nil_checker(enumeration)
+  return function(value)
+    return checks.is_enumeration_or_nil(value, enumeration)
+  end
+end
+
+---
+-- @tparam any value
+-- @tparam tab class a class created by library [middleclass](https://github.com/kikito/middleclass)
+-- @treturn bool
+function checks.is_instance(value, class)
+  assert(checks.is_table(class))
+
+  return checks.is_table(value)
+    and checks.is_callable(value.isInstanceOf)
+    and value:isInstanceOf(class)
+end
+
+---
+-- @tparam tab class a class created by library [middleclass](https://github.com/kikito/middleclass)
+-- @treturn func `func(value: any): bool`
+function checks.make_instance_checker(class)
+  return function(value)
+    return checks.is_instance(value, class)
+  end
+end
+
+---
+-- @tparam any value
+-- @tparam tab class a class created by library [middleclass](https://github.com/kikito/middleclass)
+-- @treturn bool
+function checks.is_instance_or_nil(value, class)
+  return checks.is_instance(value, class) or value == nil
+end
+
+---
+-- @tparam tab class a class created by library [middleclass](https://github.com/kikito/middleclass)
+-- @treturn func `func(value: any): bool`
+function checks.make_instance_or_nil_checker(class)
+  return function(value)
+    return checks.is_instance_or_nil(value, class)
+  end
+end
+
+function checks._without_deep_checks(deep_checks_mode)
+  assert(checks.is_deep_checks_mode(deep_checks_mode))
+
+  return _global_deep_checks_mode == "without_deep_checks"
+    or deep_checks_mode == "without_deep_checks"
+end
+
+-- we cannot check right away because at that moment the check function isn't defined
+assert(checks.is_deep_checks_mode(_global_deep_checks_mode))
+
+return checks
