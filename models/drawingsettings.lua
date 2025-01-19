2cc3098bfdd218d1b9a24e3b6aad17de046f5b87
thewizardplusplus <thewizardplusplus@yandex.ru>
2025-01-19 19:27:49 +0500

    Use the `Nameable` and `Stringifiable` mixins in the `DrawingSettings` class

--- models/drawingsettings.lua
+++ models/drawingsettings.lua
@@ -1,12 +1,18 @@
+-- luacheck: no max comment line length
+
 ---
 -- @classmod DrawingSettings
 
 local middleclass = require("middleclass")
 local assertions = require("luatypechecks.assertions")
+local Nameable = require("luaserialization.nameable")
+local Stringifiable = require("luaserialization.stringifiable")
 local Point = require("lualife.models.point")
 local CellClassification = require("biohazardcore.models.cellclassification")
 
 local DrawingSettings = middleclass("DrawingSettings")
+DrawingSettings:include(Nameable)
+DrawingSettings:include(Stringifiable)
 
 ---
 -- @table instance
@@ -31,6 +37,22 @@ function DrawingSettings:initialize(field_offset, grid_step, cell_kind)
   self.grid_step = grid_step
   self.cell_kind = cell_kind
 end
+
+---
+-- @treturn tab table with instance fields
+--   (see the [luaserialization](https://github.com/thewizardplusplus/luaserialization) library)
+function DrawingSettings:__data()
+  return {
+    field_offset = self.field_offset,
+    grid_step = self.grid_step,
+    cell_kind = self.cell_kind,
+  }
+end
+
+---
+-- @function __tostring
+-- @treturn string stringified table with instance fields
+--   (see the [luaserialization](https://github.com/thewizardplusplus/luaserialization) library)
 
 ---
 -- @tparam lualife.models.Point point
