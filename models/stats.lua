7471de7d5d4f967a2f448b0e7d4afa7bae2ff30c
thewizardplusplus <thewizardplusplus@yandex.ru>
2025-01-19 19:28:29 +0500

    Use the `Nameable` and `Stringifiable` mixins in the `Stats` class

--- models/stats.lua
+++ models/stats.lua
@@ -1,10 +1,16 @@
+-- luacheck: no max comment line length
+
 ---
 -- @classmod Stats
 
 local middleclass = require("middleclass")
 local assertions = require("luatypechecks.assertions")
+local Nameable = require("luaserialization.nameable")
+local Stringifiable = require("luaserialization.stringifiable")
 
 local Stats = middleclass("Stats")
+Stats:include(Nameable)
+Stats:include(Stringifiable)
 
 ---
 -- @table instance
@@ -23,5 +29,20 @@ function Stats:initialize(current, minimal)
   self.current = current
   self.minimal = minimal
 end
+
+---
+-- @treturn tab table with instance fields
+--   (see the [luaserialization](https://github.com/thewizardplusplus/luaserialization) library)
+function Stats:__data()
+  return {
+    current = self.current,
+    minimal = self.minimal,
+  }
+end
+
+---
+-- @function __tostring
+-- @treturn string stringified table with instance fields
+--   (see the [luaserialization](https://github.com/thewizardplusplus/luaserialization) library)
 
 return Stats
